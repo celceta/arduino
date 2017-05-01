1d3a2bbb64c9614cf6f6ea3b6fa375db8ad170ad
celceta <oss@celceta.com>
2017-05-01 23:52:58 +0900

    ひとまず

---
+++ Test/i2cscan/i2cscan.ino
@@ -0,0 +1,29 @@
+#include <Wire.h> //I2C Arduino Library
+
+int i,j;
+void setup(){
+  //Initialize Serial and I2C communications
+  Serial.begin(9600);
+  Wire.begin();
+  j = 0;
+  Serial.println("init");
+}
+
+void loop(){
+  Wire.beginTransmission(j);
+  Wire.write(0x00);
+  i = Wire.endTransmission();
+  if(i == 0){
+    Serial.print(j,HEX);
+    Serial.print(",");
+    Serial.println(i);
+  }
+  j++;
+  if(j > 0xFF){
+    Serial.println("Over");
+    while(1){
+     delay(1000);
+    } 
+  }
+  delay(25);
+}
